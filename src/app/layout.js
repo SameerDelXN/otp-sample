5eb5c554eeedf71225aa8bb88e74ac5e1f9e0d28
Sameer Gaikwad <sameergaikwad.delxn@gmail.com>
2025-07-21 11:32:09 +0530

    samplechange 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="fast2sms" content="hMx8IGCym8IbeMWB5IImiggeIPcJZf4f"/>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
